ddff956f77fc5c3e7af572a95c186ce6a1471f6c
vishnuacappella <acappella.studiov@gmail.com>
2021-11-29 21:20:36 +0530

    commit

---
+++ lib/screens/splash.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+import 'login.dart';
+
+class ScreenSplash extends StatefulWidget {
+  const ScreenSplash({Key? key}) : super(key: key);
+
+  @override
+  State<ScreenSplash> createState() => _ScreenSplashState();
+}
+
+class _ScreenSplashState extends State<ScreenSplash> {
+  @override
+  void initState() {
+    gotoLogin();
+    super.initState();
+  }
+
+  // @override
+  // void didChangeDependencies() {
+  //   // TODO: implement didChangeDependencies
+  //   super.didChangeDependencies();
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Image.asset('assets/images/img.jpg'),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+  }
+
+  Future<void> gotoLogin() async {
+    await Future.delayed(Duration(seconds: 3));
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => ScreenLogin(),
+      ),
+    );
+  }
+}
